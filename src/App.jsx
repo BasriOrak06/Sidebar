7a6fc526fddc43f418e3018ea630c7722e43ed1f
unknown <basriorak@aof.anadolu.edu.tr>
2023-11-06 13:06:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Home from './Home';
+import Modal from './Modal';
+import SideBar from './SideBar';
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <SideBar />
+    </>
+  );
+};
+export default App;
